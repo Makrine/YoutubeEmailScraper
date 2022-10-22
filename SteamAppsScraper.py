3a6369ffbdad11a1d6942aded90fb6dc3ebfeecc
Makrina Tskipurishvili <66353680+Makrine@users.noreply.github.com>
2022-10-22 20:05:01 +0400

    Update SteamAppsScraper.py

--- SteamAppsScraper.py
+++ SteamAppsScraper.py
@@ -5,7 +5,14 @@ import ast
 if __name__ == "__main__":
     # get_games(genres=["RPG", "Action", "Adventure"], number_of_games_to_get=5)
     genres_list = ast.literal_eval(sys.argv[1])
-    all_genres = bool(sys.argv[2])
+    if sys.argv[2] == 'T':
+        all_genres = True
+    elif sys.argv[2] == 'F':
+        all_genres = False
+    else:
+        print("Enter either T or F")
+        sys.exit()
+
     number = int(sys.argv[3])
     out = sys.argv[4]
     get_games(genres=genres_list, all_of_given_genres=all_genres, number_of_games_to_get=number, output_directory=out)
