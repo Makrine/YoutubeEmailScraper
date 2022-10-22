693d7ddf18648ff3e3c7972021a656b5d7e3bab7
Makto Maktavish <66353680+Makrine@users.noreply.github.com>
2022-10-22 19:54:17 +0400

    Update open_links_in_browser.py

--- open_links_in_browser.py
+++ open_links_in_browser.py
@@ -8,7 +8,9 @@ TITLE_NOT_FOUND = "NOT FOUND EMAILS CHANNELS"
 
 if __name__ == "__main__":
     directory = sys.argv[1]
+    minIndex = int(sys.argv[2])
+    maxIndex = int(sys.argv[3])
     links = pd.read_csv(directory + EMAILS_NOT_FOUND_FILE)
 
-    for link in links[TITLE_NOT_FOUND]:
+    for link in links[TITLE_NOT_FOUND][minIndex:maxIndex]:
         webbrowser.open(link)
